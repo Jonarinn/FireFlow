b6671ff0cb4777e92ad4fd1e9a0d89185b088e00
PaulJonathanBosse <118298640+PaulJonathanBosse@users.noreply.github.com>
2024-10-05 16:10:34 +0200

    V2_Background_Grid

--- Assets/Gridmanager.cs
+++ Assets/Gridmanager.cs
@@ -1,53 +1,48 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class GridManager : MonoBehaviour
 {
-    public GameObject tilePrefab; // Reference to the tile prefab
-    public int width = 100;
-    public int height = 100;
-    public float tileSpacing = 1.1f; // Slight spacing between tiles
+public GameObject tilePrefab; // Reference to the tile prefab
+public int width = 100;
+public int height = 100;
+public float tileSpacing = 1.1f; // Slight spacing between tiles
+void Start()
+{
+    GenerateGrid();
+}
 
-    void Start()
+void GenerateGrid()
+{
+    for (int x = 0; x < width; x++)
     {
-        GenerateGrid();
-    }
-
-    void GenerateGrid()
-    {
-        for (int x = 0; x < width; x++)
+        for (int y = 0; y < height; y++)
         {
-            for (int y = 0; y < height; y++)
-            {
-                // Instantiate a new tile at the correct position
-                GameObject newTile = Instantiate(tilePrefab, new Vector3(x * tileSpacing, y * tileSpacing, 0), Quaternion.identity);
-                
-                // Randomly set the color (for now, this is just for illustration)
-                Color tileColor = GetRandomColor();
-                newTile.GetComponent<SpriteRenderer>().color = tileColor;
+            // Instantiate a new tile at the correct position
+            GameObject newTile = Instantiate(tilePrefab, new Vector3(x * tileSpacing, y * tileSpacing, 0), Quaternion.identity);
 
-                // Make the tile a child of the GridManager for better organization in the Hierarchy
-                newTile.transform.parent = transform;
-            }
-        }
-    }
+            // Randomly set the color (for now, this is just for illustration)
+            Color tileColor = GetRandomColor();
+            newTile.GetComponent<SpriteRenderer>().color = tileColor;
 
-    Color GetRandomColor()
-    {
-        // Randomly assign one of three states: black, green, or red
-        int state = Random.Range(0, 3); // Random number between 0 and 2
-        switch (state)
-        {
-            case 0:
-                return Color.black;
-            case 1:
-                return Color.green;
-            case 2:
-                return Color.red;
-            default:
-                return Color.white; // Fallback
+            // Make the tile a child of the GridManager for better organization in the Hierarchy
+            newTile.transform.parent = transform;
         }
     }
 }
 
+Color GetRandomColor()
+{
+    // Randomly assign one of three states: black, green, or red
+    int state = Random.Range(0, 3); // Random number between 0 and 2
+    switch (state)
+    {
+        case 0:
+            return Color.black;
+        case 1:
+            return Color.green;
+        case 2:
+            return Color.red;
+        default:
+            return Color.white; // Fallback
+    }
+}
+}
