c56fb7e77e0cb2b9036e24fe66de1b2dbeb64b68
Florian König <koenidv@gmail.com>
2024-10-06 12:21:38 +0200

    feat: draggable camera

--- Assets/InputHandler.cs
+++ Assets/InputHandler.cs
@@ -5,6 +5,7 @@ public class InputHandler : MonoBehaviour
 {
     public GameController controller;
     public Tilemap tilemap;
+    public float considerClickDelta = 5f;
 
     // Update is called once per frame
     void Update()
@@ -12,15 +13,27 @@ public class InputHandler : MonoBehaviour
         CheckTileClicked().Let(tile => controller.OnTileClicked(tile));
     }
 
+    Vector3? clickStartPos = null;
     Vector2Int? CheckTileClicked()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            clickStartPos = Input.mousePosition;
+            return null;
+        }
+
+        if (clickStartPos != null && Input.GetMouseButtonUp(0))
+        {
+            var clickDragDelta = Vector3.Distance((Vector3)clickStartPos, Input.mousePosition);
+            clickStartPos = null;
+            if (clickDragDelta > considerClickDelta) return null;
+
             Vector2 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPos3 = tilemap.WorldToCell(worldPos);
             Vector2Int gridPos = new(gridPos3.x, gridPos3.y);
             return gridPos;
         }
+
         return null;
     }
 }
